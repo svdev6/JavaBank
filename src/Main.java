5a16fb517841aee0ff2ad96c2fc87f3e3ae837ac
Santiago Valencia Díaz <santiago.valencia6@utp.edu.co>
2024-10-21 16:55:42 -0500

    Se agregó funcionalidad

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, JavaBankATM!");
-
-        try {
-            int result = 10 / 0;
-        } catch (ArithmeticException e) {
-            System.out.println("No se puede dividir por cero!");
-        }
+        System.out.println("Welcome to JavaBankATM! Version control with Git.");
     }
 }
