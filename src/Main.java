8066b256d8196f0dde67865fb5c174ff7caf9552
Santiago Valencia Díaz <santiago.valencia6@utp.edu.co>
2024-10-21 21:27:31 -0500

    Se agregó funcionalidad de arrays

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to JavaBankATM! Version control with Git.");
+        double balance = 1000.0;
+        ArrayList<String> transactionHistory = new ArrayList<>();
+        
+        deposit(500, balance, transactionHistory);
+        withdraw(100, balance, transactionHistory);
+        
+        System.out.println("Balance final: " + balance);
+        for (String transaction : transactionHistory) {
+            System.out.println(transaction);
+        }
+    }
+
+    public static void deposit(double amount, double balance, ArrayList<String> transactionHistory) {
+        balance += amount;
+        transactionHistory.add("Deposited: $" + amount);
+    }
+
+    public static boolean withdraw(double amount, double balance, ArrayList<String> transactionHistory) {
+        if (balance >= amount) {
+            balance -= amount;
+            transactionHistory.add("Withdrew: $" + amount);
+            return true;
+        } else {
+            System.out.println("Insufficient funds");
+            return false;
+        }
     }
 }
